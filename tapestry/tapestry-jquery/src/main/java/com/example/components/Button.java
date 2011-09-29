80313fe194f27593dfcae094be625ae25846fa82
Ivan Khalopik <ikhalopik@gmail.com>
2011-09-29 18:22:16 +0300

    added javadocs to tapestry example

--- tapestry/tapestry-jquery/src/main/java/com/example/components/Button.java
+++ tapestry/tapestry-jquery/src/main/java/com/example/components/Button.java
@@ -13,15 +13,24 @@ import org.apache.tapestry5.dom.Node;
 import org.apache.tapestry5.dom.Text;
 
 /**
+ * Corresponds to jquery.ui button widgets. It is responsible for correct button rendering with all needed content and
+ * styles that will prevent content flashing during applying styles on client-side.
+ *
+ * @tapestrydoc
  * @author Ivan Khalopik
  * @since 1.0
  */
 public class Button {
-
+	private static final String EMPTY_TEXT_PLACEHOLDER = "_EMPTY_";
 	private static final String[][][] BUTTON_TYPES = {
-			{{CSSConstants.BUTTON_TEXT_ONLY, CSSConstants.BUTTON_ICON_ONLY}, {CSSConstants.BUTTON_ICON_ONLY, CSSConstants.BUTTON_ICONS_ONLY}},
-
-			{{CSSConstants.BUTTON_TEXT_ONLY, CSSConstants.BUTTON_TEXT_ICON_SECONDARY}, {CSSConstants.BUTTON_TEXT_ICON_PRIMARY, CSSConstants.BUTTON_TEXT_ICONS}}
+			{
+					{CSSConstants.BUTTON_TEXT_ONLY, CSSConstants.BUTTON_ICON_ONLY},
+					{CSSConstants.BUTTON_ICON_ONLY, CSSConstants.BUTTON_ICONS_ONLY}
+			},
+			{
+					{CSSConstants.BUTTON_TEXT_ONLY, CSSConstants.BUTTON_TEXT_ICON_SECONDARY},
+					{CSSConstants.BUTTON_TEXT_ICON_PRIMARY, CSSConstants.BUTTON_TEXT_ICONS}
+			}
 	};
 
 	@Parameter(defaultPrefix = BindingConstants.LITERAL, name = "class")
@@ -40,36 +49,66 @@ public class Button {
 		}
 	};
 
+	/**
+	 * Adds markup writer listener that processes child markup with {@link #processElement(org.apache.tapestry5.dom.Element)}
+	 * method.
+	 *
+	 * @param writer markup writer, not {@code null}
+	 */
 	@BeginRender
 	void beginRender(final MarkupWriter writer) {
 		writer.addListener(listener);
 	}
 
-
+	/**
+	 * Removes markup writer listener after render component.
+	 *
+	 * @param writer markup writer, not {@code null}
+	 */
 	@AfterRender
 	void afterRender(final MarkupWriter writer) {
 		writer.removeListener(listener);
 	}
 
+	/**
+	 * Processes every child element inside this component with applying all necessary styles and adding all needed
+	 * content.
+	 *
+	 * @param element element to process, not {@code null}
+	 */
 	private void processElement(final Element element) {
+		// get element name
 		final String elementName = element.getName();
+		// input elements processing differs from others
 		if (elementName.equals("input")) {
+			// get input type
 			final String type = element.getAttribute("type");
 			if (type != null && type.equals("submit")) {
+				// submit buttons can have only styles
 				addButtonStyle(element);
 			} else {
+				// other input types should be hided
+				// all styles will be applied to their label elements
 				element.addClassName(CSSConstants.HELPER_HIDDEN_ACCESSIBLE);
 			}
 		} else {
+			// other elements will have all needed styles and all necessary content
 			addButtonStyle(element);
 			addButtonContent(element);
 		}
 	}
 
+	/**
+	 * Adds special ui button styles to specified element.
+	 *
+	 * @param element element to add styles for, not {@code null}
+	 */
 	private void addButtonStyle(final Element element) {
+		// add specified class if bounded
 		if (className != null) {
 			element.addClassName(className);
 		}
+		// add usual button styles
 		element.addClassName(
 				CSSConstants.BUTTON,
 				CSSConstants.WIDGET,
@@ -77,32 +116,50 @@ public class Button {
 				CSSConstants.CORNER_ALL);
 	}
 
+	/**
+	 * Adds generated content to specified element. It includes button type class, label, primary and secondary items.
+	 * NOTE: It should not be applied to input elements, only for their labels.
+	 *
+	 * @param button button to generate content for, not {@code null}
+	 */
 	private void addButtonContent(final Element button) {
-		Element text = getButtonText(button);
-		final String buttonType = BUTTON_TYPES[text != null ? 1 : 0][primary != null ? 1 : 0][secondary != null ? 1 : 0];
+		// get label element
+		Element label = getText(button);
+		// determine button type according to hasText, hasPrimaryIcon, hasSecondaryIcon options
+		final String buttonType = BUTTON_TYPES[label != null ? 1 : 0][primary != null ? 1 : 0][secondary != null ? 1 : 0];
+		// add button type to class attribute
 		button.addClassName(buttonType);
-
-		if (text == null) {
-			text = button.element("span");
-			text.text("_PLACEHOLDER_");
+		// if label is not found, create empty placeholder
+		if (label == null) {
+			label = button.element("span");
+			label.text(EMPTY_TEXT_PLACEHOLDER);
 		}
-		text.addClassName(CSSConstants.BUTTON_TEXT);
+		label.addClassName(CSSConstants.BUTTON_TEXT);
 
+		// add span for primary icon
 		if (primary != null) {
 			button.element("span")
 					.addClassName(CSSConstants.BUTTON_ICON_PRIMARY, CSSConstants.ICON, primary)
-					.moveBefore(text);
+					.moveBefore(label);
 		}
+		// add span for secondary icon
 		if (secondary != null) {
 			button.element("span")
 					.addClassName(CSSConstants.BUTTON_ICON_SECONDARY, CSSConstants.ICON, secondary)
-					.moveAfter(text);
+					.moveAfter(label);
 		}
 	}
 
-	private Element getButtonText(final Element button) {
+	/**
+	 * Looks for text node inside the specified element and wraps it with {@code 'span'} element.
+	 *
+	 * @param button element to find text for, not {@code null}
+	 * @return {@code 'span'} wrapper element around founded text or {@code null} if not found
+	 */
+	private Element getText(final Element button) {
 		for (Node node : button.getChildren()) {
 			if (node instanceof Text) {
+				// return wrapped with span text
 				return node.wrap("span");
 			}
 		}
--- tapestry/tapestry-jquery/src/main/webapp/js/base.js
+++ tapestry/tapestry-jquery/src/main/webapp/js/base.js
@@ -1,19 +1,27 @@
 T5.extendInitializer({
 	"jquery.ui": function() {
 		(function($) {
+			// create buttons from every element of 'ui-button' class
 			$(".ui-button").each(function() {
+				// get element
 				var element = $(this);
+				// find element representing primary icon ang parse it's class attribute
 				var primary = element.find(".ui-button-icon-primary")
 						.removeClass("ui-button-icon-primary ui-button-icon-secondary ui-icon")
 						.attr("class");
+				// find element representing secondary icon ang parse it's class attribute
 				var secondary = element.find(".ui-button-icon-secondary")
 						.removeClass("ui-button-icon-primary ui-button-icon-secondary ui-icon")
 						.attr("class");
+				// find element representing button text and get it's content
 				var label = element.find(".ui-button-text").html();
+				// '_PLACEHOLDER_' means that button doesn't have any text
 				var text = label && label != "_PLACEHOLDER_";
+				// if element is label we should create button from input it references for
 				if (element.is("label")) {
 					element = $("#" + element.attr("for"));
 				}
+				// create button
 				element.button({
 					text: text,
 					label: label,
@@ -24,6 +32,7 @@ T5.extendInitializer({
 				});
 			});
 
+			// for demo only - create usual buttons from every element of 'button' class
 			$(".button").button();
 		})(jQuery);
 	}
